833f94549fd0d25aa48db2567a90ffd6b72b9bd1
Sashen Govender <sgov1313@gmail.com>
2025-06-09 10:12:27 +0200

    Update CountryFlagItem

--- app/src/main/java/za/co/sashen13/flagexplorer/ui/components/griditems/CountryFlagItem.kt
+++ app/src/main/java/za/co/sashen13/flagexplorer/ui/components/griditems/CountryFlagItem.kt
@@ -2,20 +2,19 @@ package za.co.sashen13.flagexplorer.ui.components.griditems
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
-import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.decode.SvgDecoder
-import za.co.sashen13.flagexplorer.R
 import za.co.sashen13.flagexplorer.data.remote.response.CountryResponse
 
 @Composable
@@ -23,54 +22,28 @@ fun CountryFlagItem(
     country: CountryResponse,
     onClick: () -> Unit
 ) {
-    val context = LocalContext.current
-
-    val imageLoader = remember {
-        ImageLoader.Builder(context)
-            .components { add(SvgDecoder.Factory()) }
-            .build()
-    }
-
-    val painter = rememberAsyncImagePainter(
-        model = country.flag,
-        imageLoader = imageLoader
-    )
-
-    val painterState = painter.state
-
     Column(
         modifier = Modifier
             .clickable(onClick = onClick)
             .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
+        val imageLoader = ImageLoader.Builder(LocalContext.current)
+            .components {
+                add(SvgDecoder.Factory())
+            }
+            .build()
+
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = country.flag,
+                imageLoader = imageLoader
+            ),
+            contentDescription = "Flag of ${country.name}",
             modifier = Modifier
                 .width(120.dp)
-                .height(80.dp)
-        ) {
-            when (painterState) {
-                is AsyncImagePainter.State.Loading -> {
-                    CircularProgressIndicator(modifier = Modifier.size(24.dp))
-                }
-                is AsyncImagePainter.State.Error -> {
-                    Image(
-                        painter = painterResource(id = R.mipmap.flag_default),
-                        contentDescription = "Flag not available for ${country.name}",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-                else -> {
-                    Image(
-                        painter = painter,
-                        contentDescription = "Flag of ${country.name}",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-            }
-        }
+                .height(80.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
